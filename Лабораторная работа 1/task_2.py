b70575eee18819f26204f7f2dd974695abf0caf5
MurrM <set827615@gmail.com>
2024-10-20 22:28:39 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,15 +1,12 @@
-# TODO Найдите количество книг, которое можно разместить на дискете
+volume=1.44
+pages=100
+lines=50
+symbols=25
+place=4
 
-volume = 1.44
-pages = 100
-lines = 50
-symbols = 25
-place = 4
-
-book = pages * lines * symbols # количество символов в одной книге
-place_for_book = book * place # размер одной книги в байтах
-
-volume_2 = volume * (1024**2) # объем дискеты в байтах
+book = pages * lines * symbols
+place_for_book = book * place
+volume_2 = volume * (1024**2)
 
 quantity = volume_2/place_for_book
 print("Количество книг, помещающихся на дискету:", round(quantity))
