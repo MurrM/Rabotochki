60316e99120f22595ea5fc8850b922b20172cafd
MurrM <set827615@gmail.com>
2024-11-16 23:31:58 +0300

    task_2.py

--- Лабораторная работа 4/task_2.py
+++ Лабораторная работа 4/task_2.py
@@ -11,11 +11,3 @@ def task() -> None:
     with open(OUTPUT_FILENAME, 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
-
-if __name__ == '__main__':
-    # Нужно для проверки
-    task()
-
-    with open(OUTPUT_FILENAME) as output_f:
-        for line in output_f:
-            print(line, end="")
